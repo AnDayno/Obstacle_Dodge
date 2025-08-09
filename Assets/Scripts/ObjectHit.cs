caeb3fbe41ed68fc1d5b459a3cec75790a612146
An <162073486+AnDayno@users.noreply.github.com>
2025-08-09 10:01:31 +0800

    Added a method to change color when onCollision

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+    MeshRenderer meshRenderer;
+    [SerializeField] Material material;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
+        meshRenderer.material.color = material.color;
         Debug.Log("Something hit the object: " + collision.gameObject.name);
     }
 }
